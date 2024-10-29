c1dab5c067650f207b0b031b14fa9bfc1fee21bf
eljabriyassine <yassinejabrio.2001@gmail.com>
2024-10-29 17:13:45 +0100

    feat: update data

--- data.js
+++ data.js
@@ -1,5 +1,24 @@
 // data.js
-const developers = [];
+const developers = [
+  {
+    name: "Alice",
+    skillLevel: 7,
+    maxHours: 40,
+    preferredTaskType: "feature",
+  },
+  {
+    name: "Bob",
+    skillLevel: 9,
+    maxHours: 30,
+    preferredTaskType: "bug",
+  },
+  {
+    name: "Charlie",
+    skillLevel: 5,
+    maxHours: 35,
+    preferredTaskType: "refactor",
+  },
+];
 
 const tasks = [
   {
